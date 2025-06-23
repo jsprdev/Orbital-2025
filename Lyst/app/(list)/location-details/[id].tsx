62fa2f853275fb2cf89702bc668a4e1075997df5
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-06-23 19:00:20 +0800

    - Added more details to details screen, including rating, number of reviews, and whether its open or not, and opening hours

--- Lyst/app/(list)/location-details/[id].tsx
+++ Lyst/app/(list)/location-details/[id].tsx
@@ -1,5 +1,5 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import { View, Text, Image, TouchableOpacity, Linking, ScrollView, ActivityIndicator } from "react-native";
 import { useEffect, useState } from "react";
 import { Note } from "@/types";
 import { getNotes } from "@/utils/api";
@@ -59,14 +59,11 @@ export default function LocationDetails() {
         <Stack.Screen
             options={{
                 headerShown: false, // hide header for now for our design
-                headerTitle: 'Your Saved Location',
-                headerStyle: { backgroundColor: '#222' },
-                headerTitleStyle: { color: '#fff', fontSize: 18 },
-                headerTintColor: '#fff',      // back button color
+                
             }}
         />
         
-        <View>
+        <ScrollView>
             {placeDetails?.geometry?.location && (
                 <View>
                     <TouchableOpacity
@@ -102,7 +99,7 @@ export default function LocationDetails() {
                         <Text className="text-3xl mx-6 font-bold text-black-500 mb-1">
                             {placeDetails.name}
                         </Text>
-                        <View className="flex-row items-center mx-6 space-x-4">
+                        <View className="flex-row items-center mx-6 gap-x-2">
                             <Text className="text-lg text-black font-semibold">{placeDetails.rating} ★</Text>
                             <Text className="text-base text-gray-600">
                                 ({placeDetails.user_ratings_total} reviews)
@@ -120,32 +117,65 @@ export default function LocationDetails() {
                         source={{
                         uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${placeDetails.photos[0].photo_reference}&key=${process.env.EXPO_PUBLIC_GOOGLE_PLACES_API_KEY}`
                         }}
-                        className="w-full h-64 rounded-xl mb-3"
+                        className="w-full h-64 mb-3"
                         resizeMode="cover"
                     />
                     )}
                     
-                    
-                    <Text className="text-xl text-black-500 mx-6">
-                        {placeDetails.formatted_address}
-                    </Text>
-                    <View className="text-sm px-6">
-                        {/* since some locations they input might not have official opening hours yet */}
-                        {placeDetails.current_opening_hours?.weekday_text.map((line: string, idx: number) => (
-                            <Text key={idx} className="text-sm text-black-500">{line}</Text>
-                            
-                        ))}
+                    <View className="gap-y-1 px-6">
+                        <Text className="text-xl font-bold text-black-500">
+                            Address
+                        </Text>
+                        <Text className="text-md text-black-500">
+                            {placeDetails.formatted_address}
+                        </Text>
+                    </View>
+                    <View className="px-6 gap-y-1">
+                        <Text className="text-xl font-bold text-black-500">
+                            Opening Hours
+                        </Text>
+                        <View>
+                            {placeDetails.current_opening_hours?.weekday_text ? placeDetails.current_opening_hours?.weekday_text.map((line: string, idx: number) => {
+                                const [day, hours] = line.split(': ');
+                                // some places have multiple time ranges that they are open, maybe close for lunch break etc
+                                const hourRanges = hours ? hours.split(', ') : ['']; 
+
+                                return (
+                                    <View key={idx}>
+                                        {hourRanges.map((range, i) => (
+                                            <View key={i} className="flex-row">
+                                                {/* to add whitespace to the day name */}
+                                                <View className="w-28"> 
+                                                    {/* for the first "row" display the day name, but aft that just put whitespace */}
+                                                {i === 0 ? (
+                                                    <Text className="text-sm text-gray-500">{day}</Text>
+                                                ) : (
+                                                    <Text className="text-sm text-black-500">{' '}</Text>
+                                                )}
+                                                </View>
+                                                <View className="flex-1">
+                                                    <Text className="text-sm text-black-500">{range}</Text>
+                                                </View>
+                                            </View>
+                                         ))}
+                                    </View>
+                                );
+                            })
+                            : <Text className="text-gray-500">No available information</Text>}
+                        </View>
                     </View>
                 
                     
                 </View>
                 
             ) : (
-                <Text>Loading place details...</Text>
+                <View className="flex-1 justify-center items-center min-h-[200px]">
+                    <ActivityIndicator size="large" color="#22c55e" />
+                </View>
             )}
 
             
-        </View>
+        </ScrollView>
         </>
     );
 }
