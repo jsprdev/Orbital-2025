7e0bb509f94c7dbdc34a5729718e0d9a8ada569a
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-07-21 15:18:59 +0800

    Reverted back to local testing

--- Lyst/utils/index.ts
+++ Lyst/utils/index.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // AXIOS Config
 const axiosInstance = axios.create({
-  baseURL: `https://${process.env.EXPO_PUBLIC_HOST}`,
+  // baseURL: `https://${process.env.EXPO_PUBLIC_HOST}`,
+  baseURL: `http://${process.env.EXPO_PUBLIC_HOST}:${process.env.EXPO_PUBLIC_PORT}`,
   headers: {
     "Content-Type": "application/json",
   },
